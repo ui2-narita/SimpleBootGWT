0f28671ae7a963e8a2bb118d67dded6a9a24b3a7
成田英樹 <hideki.narita@ui2.co.jp>
2016-07-14 18:40:09 +0900

    アプリケーション雛形の作成

---
+++ src/main/java/jp/co/ui2/simple/App.java
@@ -0,0 +1,15 @@
+package jp.co.ui2.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
