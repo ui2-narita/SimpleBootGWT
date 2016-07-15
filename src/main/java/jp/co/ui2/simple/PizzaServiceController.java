027e8c35fd123309c85f3100249115b29ea6c198
成田英樹 <hideki.narita@ui2.co.jp>
2016-07-15 16:20:59 +0900

    REST API 追加( order )

---
+++ src/main/java/jp/co/ui2/simple/PizzaServiceController.java
@@ -0,0 +1,39 @@
+package jp.co.ui2.simple;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import jp.co.ui2.shared.OrderConfirmation;
+import jp.co.ui2.shared.PizzaOrder;
+import jp.co.ui2.shared.Topping;
+import lombok.val;
+
+@RestController
+@RequestMapping("/simplegwt/pizza-service")
+public class PizzaServiceController {
+  
+  private static List<OrderConfirmation> orders = new LinkedList<>();
+  
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<OrderConfirmation> order(PizzaOrder order
+      , UriComponentsBuilder uriComponentsBuilder) {
+    
+    OrderConfirmation confirmation = new OrderConfirmation();
+    confirmation.order_id = 123123;
+    confirmation.order    = order;
+    confirmation.price    = 27.54;
+    confirmation.ready_time = System.currentTimeMillis() + 1000 * 60 * 30; // in  30 min.
+    
+    val headers = new HttpHeaders();
+    headers.setLocation(uriComponentsBuilder.path("/pizza-service/{id}").buildAndExpand(orders.size()).toUri());
+    return new ResponseEntity<OrderConfirmation>(confirmation, headers, HttpStatus.CREATED);
+  }
+}
