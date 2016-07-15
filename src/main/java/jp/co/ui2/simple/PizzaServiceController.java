1798bcbb98f437caadcd1f01148396285ec77a3b
成田英樹 <hideki.narita@ui2.co.jp>
2016-07-15 18:36:48 +0900

    JSON  - Object 変換を修正

--- src/main/java/jp/co/ui2/simple/PizzaServiceController.java
+++ src/main/java/jp/co/ui2/simple/PizzaServiceController.java
@@ -1,15 +1,21 @@
 package jp.co.ui2.simple;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jp.co.ui2.shared.OrderConfirmation;
 import jp.co.ui2.shared.PizzaOrder;
@@ -23,8 +29,11 @@ public class PizzaServiceController {
   private static List<OrderConfirmation> orders = new LinkedList<>();
   
   @RequestMapping(method = RequestMethod.POST)
-  public ResponseEntity<OrderConfirmation> order(PizzaOrder order
-      , UriComponentsBuilder uriComponentsBuilder) {
+  public ResponseEntity<OrderConfirmation> order(@RequestBody String orderJson
+      , UriComponentsBuilder uriComponentsBuilder) throws JsonParseException, JsonMappingException, IOException {
+    
+    ObjectMapper mapper = new ObjectMapper();
+    PizzaOrder order = mapper.readValue(orderJson, PizzaOrder.class);
     
     OrderConfirmation confirmation = new OrderConfirmation();
     confirmation.order_id = 123123;
